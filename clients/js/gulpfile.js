ee3a020427ff517169a05bf2cdf997fd13c16e6a
Shane F. Carr <sffc@google.com>
2017-11-03 22:14:47 -0700

    Adding JavaScript compatibility test and support for browser testing

--- clients/js/gulpfile.js
+++ clients/js/gulpfile.js
@@ -1,39 +1,65 @@
-const fs = require("fs");
-const clone = require("gulp-clone");
-const gulp = require("gulp");
-const preprocess = require("gulp-preprocess");
-const rename = require("gulp-rename");
-const replace = require("gulp-replace");
-const ts = require("gulp-typescript");
-const tsProject = ts.createProject("tsconfig.json");
-const uglify = require("gulp-uglify");
+var fs = require("fs");
+var clean = require("gulp-clean");
+var gulp = require("gulp");
+var jasmine = require("gulp-jasmine");
+var jasmineBrowser = require("gulp-jasmine-browser");
+var preprocess = require("gulp-preprocess");
+var rename = require("gulp-rename");
+var replace = require("gulp-replace");
+var ts = require("gulp-typescript");
+var uglify = require("gulp-uglify");
 
-gulp.task("default", function () {
-    var js = tsProject.src()
+// Helper function to create a non-minified JavaScript stream.
+function typescriptToJavascript() {
+    var tsProject = ts.createProject("tsconfig.json");
+    return tsProject.src()
         .pipe(replace(
             "<BASE64_DATA_PLACEHOLDER>",
             fs.readFileSync("resources/zawgyiUnicodeModel.dat", "base64")
         ))
         .pipe(tsProject())
         .js;
+}
 
-    // Save human-readable JavaScript for Node
-    js.pipe(clone())
+gulp.task("build-node", function () {
+    return typescriptToJavascript()
         .pipe(preprocess({
             context: { NODEJS: true }
         }))
         .pipe(gulp.dest("build"));
+});
 
-    // Save minified JavaScript for browsers
-    js
+gulp.task("build-browser", function () {
+    return typescriptToJavascript()
         .pipe(preprocess({
             context: { NODEJS: false }
         }))
         .pipe(uglify({
-            wrap: "googlei18n"
+            wrap: "google_myanmar_tools"
         }))
         .pipe(rename({
             extname: ".min.js"
         }))
         .pipe(gulp.dest("build"));
 });
+
+gulp.task("test-node", ["build-node"], function () {
+    return gulp.src("spec/**/*_spec.js")
+        .pipe(jasmine());
+});
+
+gulp.task("test-browser", ["build-browser"], function () {
+    return gulp.src([
+        "build/zawgyi_detector.min.js",
+        "resources/compatibility.tsv",
+        "spec/**/*_spec.js"])
+        .pipe(jasmineBrowser.specRunner({ console: true }))
+        .pipe(jasmineBrowser.headless({ driver: "phantomjs" }));
+});
+
+gulp.task("clean", function () {
+    return gulp.src("build").pipe(clean());
+});
+
+gulp.task("test", ["test-node", "test-browser"]);
+gulp.task("default", ["build-node", "build-browser"]);
--- clients/js/spec/zawgyi_detector_spec.js
+++ clients/js/spec/zawgyi_detector_spec.js
@@ -1,11 +1,67 @@
-const ZawgyiDetector = require("../build/zawgyi_detector").ZawgyiDetector;
+// It would be nice to use preprocessor flags here, but that would require
+// building this file before Jasmine can run it.
+
+var ZawgyiDetector, compatibilityTSV;
+if (typeof process !== "undefined") {
+    // NodeJS
+    ZawgyiDetector = require("../build/zawgyi_detector").ZawgyiDetector;
+    compatibilityTSV = require("fs").readFileSync("resources/compatibility.tsv", "utf-8");
+} else {
+    // Browser
+    ZawgyiDetector = window.google_myanmar_tools.ZawgyiDetector;
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "compatibility.tsv", false);
+    xhr.send(null);
+    compatibilityTSV = xhr.responseText;
+}
+
+function compareFloatingPoint(actual, expected, tolerance, info) {
+    const message = "Expected " + actual + " to be within " + tolerance + " relative error of " + expected + ": " + info;
+    if (actual === expected) {
+        return {
+            pass: true,
+            message: message
+        };
+    }
+    const relativeError = Math.abs((actual - expected) / expected);
+    return {
+        pass: relativeError <= tolerance,
+        message: message
+    };
+}
 
 describe("ZawgyiDetector Behavior Tests", function () {
-    it("strong", function () {
+    it("should produce expected results on strings with strong signals", function () {
         const detector = new ZawgyiDetector();
-        const strong_unicode = "အပြည်ပြည်ဆိုင်ရာ လူ့အခွင့်အရေး ကြေညာစာတမ်း";
-        const strong_zawgyi = "အျပည္ျပည္ဆိုင္ရာ လူ႔အခြင့္အေရး ေၾကညာစာတမ္း";
-        expect(detector.getZawgyiProbability(strong_unicode)).toBeLessThan(0.001);
-        expect(detector.getZawgyiProbability(strong_zawgyi)).toBeGreaterThan(0.999);
+        const strongUnicode = "အပြည်ပြည်ဆိုင်ရာ လူ့အခွင့်အရေး ကြေညာစာတမ်း";
+        const strongZawgyi = "အျပည္ျပည္ဆိုင္ရာ လူ႔အခြင့္အေရး ေၾကညာစာတမ္း";
+        expect(detector.getZawgyiProbability(strongUnicode)).toBeLessThan(0.001);
+        expect(detector.getZawgyiProbability(strongZawgyi)).toBeGreaterThan(0.999);
+    });
+});
+
+describe("ZawgyiDetector Compatibility Tests", function () {
+
+    beforeEach(function () {
+        jasmine.addMatchers({
+            toEqualFloatingPoint: function () {
+                return { compare: compareFloatingPoint }
+            }
+        });
+    });
+
+    it("should produce the same resuls as in compatibility.csv", function () {
+        const detector = new ZawgyiDetector();
+        compatibilityTSV.split("\n").forEach(function (line) {
+            if (line.length === 0) return;
+            const tabIndex = line.indexOf("\t");
+            const floatString = line.substr(0, tabIndex);
+            const expectedProbability = (floatString === "-Infinity")
+                ? Number.NEGATIVE_INFINITY
+                : parseFloat(floatString);
+            const testInput = line.substr(tabIndex + 1);
+            expect(detector.getZawgyiProbability(testInput))
+                .toEqualFloatingPoint(expectedProbability, 1e-6, testInput);
+        }, this);
     });
 });
