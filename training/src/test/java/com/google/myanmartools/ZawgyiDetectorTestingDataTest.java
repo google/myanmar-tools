b63927b4bcbc25ba4b3f813e4e084caa445ce7ab
Shane Carr <sffc@google.com>
2018-06-19 22:16:40 -0700

    Adding missing JUnit @RunWith annotation

--- training/src/test/java/com/google/myanmartools/ZawgyiDetectorTestingDataTest.java
+++ training/src/test/java/com/google/myanmartools/ZawgyiDetectorTestingDataTest.java
@@ -28,7 +28,10 @@ import java.net.URL;
 import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
+@RunWith(JUnit4.class)
 public class ZawgyiDetectorTestingDataTest {
 
   private static ZawgyiDetector detector;
